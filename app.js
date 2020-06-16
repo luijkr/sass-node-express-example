dbf382646957a2dd5329e0fe84c0e1257b7967f1
Rene Luijk <rene.luijk@sanoma.com>
2020-06-16 17:57:25 +0200

    Start pugifying

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const index = require('./routes/index');
 const app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
